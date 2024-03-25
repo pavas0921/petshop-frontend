753d6a563fabbdf2a47a1c6271e46f1ce21f41bb
Carlos Andrés Pavas Correa <pavas0921@gmail.com>
2024-03-25 16:19:27 -0500

    Se corrige menu navbar para pantallas mobiles

--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -117,7 +117,6 @@ const NavbarComponent = () => {
     setAnchorProductos(null);
   };
 
-  
   const handleCloseVender = () => {
     setAnchorVender(null);
   };
@@ -150,158 +149,97 @@ const NavbarComponent = () => {
               <Button
                 color="inherit"
                 onClick={handleClickPruebas}
-                sx={{ fontSize: "1rem" }}
+                sx={{
+                  fontWeight: "bold",
+                  textTransform: "capitalize",
+                  fontSize: "1rem",
+                }}
               >
-                Administracion
+                Administración
               </Button>
+
+              <Button
+                color="inherit"
+                onClick={handleClickProductos}
+                sx={{
+                  fontWeight: "bold",
+                  textTransform: "capitalize",
+                  fontSize: "1rem",
+                }}
+              >
+                Productos
+              </Button>
+
               <Menu
-                anchorEl={anchorPruebas}
-                open={Boolean(anchorPruebas)}
-                onClose={handleClosePruebas}
+                anchorEl={anchorProductos}
+                open={Boolean(anchorProductos)}
+                onClose={handleCloseProductos}
               >
                 <MenuItem onClick={() => navigate("/products")}>
                   Productos
                 </MenuItem>
               </Menu>
+
+              <Button
+                color="inherit"
+                onClick={handleClickVender}
+                sx={{
+                  fontWeight: "bold",
+                  textTransform: "capitalize",
+                  fontSize: "1rem",
+                }}
+              >
+                Vender
+              </Button>
+
+              <Menu
+                anchorEl={anchorVender}
+                open={Boolean(anchorVender)}
+                onClose={handleCloseVender}
+              >
+                <MenuItem onClick={() => navigate("/basic-sale")}>
+                  Venta Básica
+                </MenuItem>
+              </Menu>
+
               <Menu
                 anchorEl={anchorPruebas}
                 open={Boolean(anchorPruebas)}
                 onClose={handleClosePruebas}
               >
-                <MenuItem onClick={() => navigate("/products")}>
-                  Clientes
+                <MenuItem onClick={handleClickUsuarios}>
+                  Usuarios
+                  <ListItemIcon>
+                    <ArrowDropDownIcon />
+                  </ListItemIcon>
                 </MenuItem>
               </Menu>
-
-              <Button
-                color="inherit"
-                onClick={handleClickAgenda}
-                sx={{ fontSize: "1rem" }}
-              >
-                Agenda
-              </Button>
               <Menu
-                anchorEl={anchorAgenda}
-                open={Boolean(anchorAgenda)}
-                onClose={handleCloseAgenda}
+                anchorEl={anchorUsuarios} // Agregar este estado para el submenu de Usuarios
+                open={Boolean(anchorUsuarios)}
+                onClose={handleCloseUsuarios}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right", // Ajusta esto a 'left' si deseas que se abra hacia la izquierda
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
               >
-                <MenuItem onClick={() => navigate("/agenda")}>Agenda</MenuItem>
-                <MenuItem onClick={handleCloseAgenda}>
-                  Bloqueo de Agenda
-                </MenuItem>
-                <MenuItem onClick={handleCloseAgenda}>
-                  Eventos por Médico
-                </MenuItem>
-              </Menu>
-
-              <Button color="inherit" sx={{ fontSize: "1rem" }}>
-                Historia Clinica
-              </Button>
-
-              <Button
-                color="inherit"
-                onClick={handleClickFacturacion}
-                sx={{ fontSize: "1rem" }}
-              >
-                Facturacion
-              </Button>
-              <Menu
-                anchorEl={anchorFacturacion}
-                open={Boolean(anchorFacturacion)}
-                onClose={handleCloseFacturacion}
-              >
-                <MenuItem onClick={handleCloseFacturacion}>
-                  Facturar a Terceros
-                </MenuItem>
-                <MenuItem onClick={handleCloseFacturacion}>
-                  Maestro Facturas
-                </MenuItem>
-                <MenuItem onClick={handleCloseFacturacion}>
-                  Maestro Recibos de Pagos
-                </MenuItem>
-                <MenuItem onClick={handleCloseFacturacion}>
-                  Cuadre de Caja
-                </MenuItem>
-                <MenuItem onClick={handleCloseFacturacion}>
-                  Maestro Convenios
-                </MenuItem>
-                <MenuItem onClick={handleCloseFacturacion}>
-                  Liberar Evento
-                </MenuItem>
-              </Menu>
-              <Button
-                color="inherit"
-                onClick={handleClickCartera}
-                sx={{ fontSize: "1rem" }}
-              >
-                Cartera
-              </Button>
-              <Menu
-                anchorEl={anchorCartera}
-                open={Boolean(anchorCartera)}
-                onClose={handleCloseCartera}
-              >
-                <MenuItem onClick={handleCloseCartera}>
-                  Maestro Notas Crédito
-                </MenuItem>
-                <MenuItem onClick={handleCloseCartera}>
-                  Maestro Notas Débito
-                </MenuItem>
-              </Menu>
-              <Button
-                color="inherit"
-                onClick={handleClickRips}
-                sx={{ fontSize: "1rem" }}
-              >
-                RIPS
-              </Button>
-              <Menu
-                anchorEl={anchorRips}
-                open={Boolean(anchorRips)}
-                onClose={handleCloseRips}
-              >
-                <MenuItem onClick={handleCloseRips}>Generar RIPS EPS</MenuItem>
-                <MenuItem onClick={handleCloseRips}>
-                  Generar RIPS Extranjeros
-                </MenuItem>
-                <MenuItem onClick={handleCloseRips}>Maestro RIPS</MenuItem>
-              </Menu>
-              <Button
-                color="inherit"
-                onClick={handleClickInformes}
-                sx={{ fontSize: "1rem" }}
-              >
-                <Typography>Informes</Typography>
-              </Button>
-              <Menu
-                anchorEl={anchorInformes}
-                open={Boolean(anchorInformes)}
-                onClose={handleCloseInformes}
-              >
-                <MenuItem onClick={handleCloseInformes}>Diagnosticos</MenuItem>
-                <MenuItem onClick={handleCloseInformes}>Oportunidad</MenuItem>
-                <MenuItem onClick={handleCloseInformes}>Facturación</MenuItem>
-              </Menu>
-              <Button
-                color="inherit"
-                onClick={handleClickAdministracion}
-                sx={{ fontSize: "1rem" }}
-              >
-                ADMINISTRACION
-              </Button>
-              <Menu
-                anchorEl={anchorAdministracion}
-                open={Boolean(anchorAdministracion)}
-                onClose={handleCloseAdministracion}
-              >
-                <MenuItem onClick={() => navigate("/user-add")}>
+                <MenuItem onClick={() => navigate("/user/register")}>
                   Registrar Usuario
                 </MenuItem>
                 <MenuItem onClick={() => navigate("/user-list")}>
                   Maestro de Usuarios
                 </MenuItem>
-                <MenuItem onClick={() => navigate("/administadoras-de-salud")}>
-                  Administradoras de Salud
+
+                <MenuItem onClick={() => navigate("/products")}>
+                  Productos
+                </MenuItem>
+
+                <MenuItem onClick={() => navigate("/customer")}>
+                  Clientes
                 </MenuItem>
               </Menu>
             </Drawer>
@@ -362,7 +300,6 @@ const NavbarComponent = () => {
                   Venta Básica
                 </MenuItem>
               </Menu>
-
 
               <Menu
                 anchorEl={anchorPruebas}
