11d5411b82b9353843fc2a55fe62df049ae00178
desarrollo4@historyclinical.com <desarrollo4@historyclinical.com>
2024-04-12 13:56:29 -0500

    Se agrega integración con el endpoint de proveedores para consulta y creación

---
+++ src/features/supplier/supplierSlice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { getAllSupplierAPI, createSupplierAPI } from '../../services/supplier'
+
+const initialState = {
+  suppliers: [],
+  supplierLoading: false,
+  httpStatus: null,
+}
+
+export const getAllSupplier = createAsyncThunk('get/supplier', async () => {
+  const data = await getAllCompaniesAPI()
+  return data
+})
+
+export const createSupplier = createAsyncThunk(
+  'post/createSupplierAPI',
+  async (body) => {
+    const data = await createSupplierAPI(body)
+    return data
+  }
+)
+
+export const supplierSlice = createSlice({
+  name: 'suppliers',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllSupplier.pending, (state) => {
+        state.supplierLoading = true
+      })
+      .addCase(getAllSupplier.fulfilled, (state, action) => {
+        state.supplierLoading = false
+        state.supplierLoading = action.payload.suppliers
+        state.httpStatus = action.payload.status
+      })
+      .addCase(createSupplier.pending, (state) => {
+        state.supplierLoading = true
+      })
+      .addCase(createSupplier.fulfilled, (state, action) => {
+        state.supplierLoading = false
+        state.supplierLoading = action.payload.suppliers
+        state.httpStatus = action.payload.status
+      })
+  },
+})
+
+export const selectSupplierState = (state) => state.suppliers
+export default supplierSlice.reducer
