74ad680c0158c38b5a7b25858ff51967fa5cc249
Andres Pavas <andrespavas@MacBook-Air-de-Andres.local>
2024-10-30 19:20:38 -0500

    update

--- src/services/customer.js
+++ src/services/customer.js
@@ -1,5 +1,6 @@
-const API_BASE_URL = "https://muezzapets-backend.onrender.com";
+//const API_BASE_URL = "https://muezzapets-backend.onrender.com";
 //const API_BASE_URL = "http://localhost:4000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const getCustomerByCompanyAPI = async (companyId) => {
     try {
